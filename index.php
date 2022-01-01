98606803ff42362a726e1c80035a91930dfe10d1
KoalaHuang <hongjian.huang@gmail.com>
2022-01-01 09:39:55 +0800

    test db connection

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost:3307";
+$username = "coffeedb";
+$password = "koala5@FR11";
+$dbname = "coffeedb";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "SELECT * FROM `t_cat`";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "Category: " . $row["c_cat"] . "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
